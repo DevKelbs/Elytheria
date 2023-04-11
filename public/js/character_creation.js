7a7c39a3ed41e49a1b2a8de70e9c3f676be95007
DevKelbs <130209745+DevKelbs@users.noreply.github.com>
2023-04-10 17:53:59 -0700

    troubleshooting.1

--- public/js/character_creation.js
+++ public/js/character_creation.js
@@ -1,20 +1,22 @@
 const socket = io();
-const createCharacterButton = document.getElementById('createCharacterButton');
 const characterNameInput = document.getElementById('characterName');
 const characterRaceSelect = document.getElementById('characterRace');
 
-createCharacterButton.addEventListener('click', () => {
-    const characterName = characterNameInput.value;
-    const characterRace = characterRaceSelect.value;
+document.addEventListener("DOMContentLoaded", () => {
+    const createCharacterButton = document.getElementById("createCharacter");
+    createCharacterButton.addEventListener('click', () => {
+        const characterName = characterNameInput.value;
+        const characterRace = characterRaceSelect.value;
 
-    if (!characterName || !characterRace) {
-        alert('Please enter a character name and select a race.');
-        return;
-    }
+        if (!characterName || !characterRace) {
+            alert('Please enter a character name and select a race.');
+            return;
+        }
 
-    socket.emit('createCharacter', {
-        userId: localStorage.getItem('userID'),
-        name: characterName,
-        race: characterRace,
+        socket.emit('createCharacter', {
+            userId: localStorage.getItem('userID'),
+            name: characterName,
+            race: characterRace,
+        });
     });
 });
