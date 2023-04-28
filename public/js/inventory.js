0718e39743057a8e0c47c8e879b0fe6e69770a96
kcorr <kcorr1@live.com>
2023-04-28 10:38:44 -0500

    styling inv view

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,58 +1,76 @@
 function getItemImageSrc(itemName) {
-    switch (itemName) {
-      case "NormalTreeLog":
-        return "./assets/images/objects/leaf1.png";
-      case "OakTreeLog":
-        return "./assets/images/objects/leaf2.png";
-      // Add more cases for different item names and their corresponding images
-      default:
-        return "./assets/icons/skill-icons/icon_woodcutting_elvish.png";
-    }
+  switch (itemName) {
+    case "NormalTreeLog":
+      return "./assets/images/objects/leaf1.png";
+    case "OakTreeLog":
+      return "./assets/images/objects/leaf2.png";
+    // Add more cases for different item names and their corresponding images
+    default:
+      return "./assets/icons/skill-icons/icon_woodcutting_elvish.png";
   }
-  
+}
 
- export function updateInventoryDisplay() {
-    const activeCharacter = JSON.parse(localStorage.getItem("activeCharacter")) || {};
-    const inventory = activeCharacter.inventory || {};
-    const inventoryContainer = document.getElementById("inventoryView");
-  
-    // Clear the inventory container
-    inventoryContainer.innerHTML = "";
-  
-    // Iterate through the inventory items
-    for (const [item, quantity] of Object.entries(inventory)) {
-      // Create an inventory item element
-      const inventoryItem = document.createElement("li");
-      inventoryItem.classList.add("inventory-item");
-  
-      // Add item image
-      const itemImage = document.createElement("img");
-      itemImage.src = getItemImageSrc(item);
-      itemImage.alt = `${item} image`;
-      itemImage.classList.add("inventory-item-image"); // Add a class for image styling
-      inventoryItem.appendChild(itemImage);
-  
-      // Add item name
-      const itemName = document.createElement("div");
-      itemName.classList.add("inventory-item-name");
-      itemName.textContent = item;
-      inventoryItem.appendChild(itemName);
-  
-      // Add item quantity
-      const itemQuantity = document.createElement("div");
-      itemQuantity.classList.add("inventory-item-quantity");
-      itemQuantity.textContent = `Quantity: ${quantity}`;
-      inventoryItem.appendChild(itemQuantity);
-  
-      // Append the inventory item to the container
-      inventoryContainer.appendChild(inventoryItem);
-    }
+export function updateInventoryDisplay() {
+  const activeCharacter =
+    JSON.parse(localStorage.getItem("activeCharacter")) || {};
+  const inventory = activeCharacter.inventory || {};
+  const inventoryContainer = document.getElementById("inventoryView");
+
+  // Clear the inventory container
+  inventoryContainer.innerHTML = "";
+
+  // Iterate through the inventory items
+  for (const [item, quantity] of Object.entries(inventory)) {
+    // Create an inventory item element
+    const inventoryItem = document.createElement("li");
+    inventoryItem.classList.add("inventory-item");
+
+    // Add item image
+    const itemImage = document.createElement("img");
+    itemImage.src = getItemImageSrc(item);
+    itemImage.alt = `${item} image`;
+    itemImage.classList.add("inventory-item-image"); // Add a class for image styling
+    inventoryItem.appendChild(itemImage);
+
+    // Add item name
+    // const itemName = document.createElement("div");
+    // itemName.classList.add("inventory-item-name");
+    // itemName.textContent = item;
+    // inventoryItem.appendChild(itemName);
+
+    // Add item quantity
+    const itemQuantity = document.createElement("div");
+    itemQuantity.classList.add("inventory-item-quantity");
+    itemQuantity.textContent = `${quantity}`;
+    inventoryItem.appendChild(itemQuantity);
+
+    // Create tooltip container
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("item-tooltip");
+
+    // Add tooltip image
+    const tooltipImage = document.createElement("img");
+    tooltipImage.src = getItemImageSrc(item);
+    tooltipImage.alt = `${item} image`;
+    tooltipImage.classList.add("tooltip-image");
+    tooltip.appendChild(tooltipImage);
+
+    // Add tooltip text
+    const tooltipText = document.createElement("span");
+    tooltipText.classList.add("tooltip-text");
+    tooltipText.textContent = item;
+    tooltip.appendChild(tooltipText);
+
+    // Add tooltip to the inventory item
+    inventoryItem.appendChild(tooltip);
+
+    // Append the inventory item to the container
+    inventoryContainer.appendChild(inventoryItem);
   }
-  
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    updateInventoryDisplay();
-  });
-  
-  // Call updateInventoryDisplay() after updating the inventory in the runTask function
-  
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateInventoryDisplay();
+});
+
+// Call updateInventoryDisplay() after updating the inventory in the runTask function
