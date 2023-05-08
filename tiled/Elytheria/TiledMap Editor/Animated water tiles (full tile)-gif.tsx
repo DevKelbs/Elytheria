a5e677a6f7d3ab3c2fe3b10d79043469ff5e58b4
DevKelbs <130209745+DevKelbs@users.noreply.github.com>
2023-05-08 11:18:32 -0700

    pushhhhhhhhhhhhh

---
+++ tiled/Elytheria/TiledMap Editor/Animated water tiles (full tile)-gif.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Animated water tiles(edges)-style1" tilewidth="32" tileheight="32" tilecount="672" columns="14">
+ <image source="../Tilesets/Animated water tiles(edges)-style1.png" width="448" height="1536"/>
+</tileset>
